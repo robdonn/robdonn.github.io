6f8cf04a592a94e3e0f19b9753dfaa801674486d
Robert Donnelly <dev@robdonn.com>
2019-01-04 08:50:37 +0000

    Logos

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import "./Header.css";
 
+import { ReactComponent as Code } from "./SVG/code.svg";
+import { ReactComponent as Vids } from "./SVG/vids.svg";
+import { ReactComponent as Health } from "./SVG/health.svg";
+
+const icons = {
+  Code,
+  Vids,
+  Health
+};
+
 export class Header extends Component {
   state = {
-    selected: null
+    selected: null,
+    sections: this.props.sections.reduce(
+      (acc, curr) => ({
+        ...acc,
+        [curr.toLowerCase()]: {
+          name: curr,
+          nameLower: curr.toLowerCase()
+        }
+      }),
+      {}
+    )
   };
 
   handleClick = e => {
@@ -16,23 +36,25 @@ export class Header extends Component {
   };
 
   render() {
-    const { sections } = this.props;
-    const { selected } = this.state;
+    const { sections, selected } = this.state;
     return (
       <header className={`Header ${selected ? "selected" : ""}`}>
         <ul className="Header-list">
-          {sections.map(section => (
-            <li
-              key={section}
-              data-section={section}
-              onClick={this.handleClick}
-              className={`Header-list-item ${section.toLowerCase()} ${
-                section === selected ? "selected" : ""
-              }`}
-            >
-              {section}
-            </li>
-          ))}
+          {Object.values(sections).map(({ name, nameLower }) => {
+            const Icon = icons[name];
+            return (
+              <li
+                key={nameLower}
+                data-section={nameLower}
+                onClick={this.handleClick}
+                className={`Header-list-item ${nameLower} ${
+                  nameLower === selected ? "selected" : ""
+                }`}
+              >
+                <Icon className="Header-list-logo" />
+              </li>
+            );
+          })}
         </ul>
         <button onClick={this.handleClose} className="Header-close-button">
           Close
